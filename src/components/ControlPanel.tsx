097ebdd1aaade7c216c6e76526552926d8bc9780
TraianC299 <traianciochina@gmail.com>
2025-01-08 21:47:52 +0200

    more improvements

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -17,16 +17,16 @@ const ControlPanel = ({
     return (
         <div className='w-64 h-full flex flex-col border-r-[1px]'>
             <div className='p-4 flex flex-col gap-2'>
-                <p className='text-2xl font-semibold'>Lissajous</p>
-                <Input label='a' value={`${form.amplitudeX}`} onChange={(e) => changeForm(e.target.value, 'amplitudeX')} />
-                <Input label='b' value={`${form.amplitudeY}`} onChange={(e) => changeForm(e.target.value, 'amplitudeY')} />
-                <Input label='A' value={`${form.frequencyX}`} onChange={(e) => changeForm(e.target.value, 'frequencyX')} />
-                <Input label='B' value={`${form.frequencyY}`} onChange={(e) => changeForm(e.target.value, 'frequencyY')} />
+                <p className='text-2xl font-semibold'>Lissajous figure</p>
+                <Input label='Amplitude X' value={`${form.amplitudeX}`} onChange={(e) => changeForm(e.target.value, 'amplitudeX')} />
+                <Input label='Amplitude Y' value={`${form.amplitudeY}`} onChange={(e) => changeForm(e.target.value, 'amplitudeY')} />
+                <Input label='Frequency X' value={`${form.frequencyX}`} onChange={(e) => changeForm(e.target.value, 'frequencyX')} />
+                <Input label='Frequency Y' value={`${form.frequencyY}`} onChange={(e) => changeForm(e.target.value, 'frequencyY')} />
                 <Input label='Phase' value={`${phi.toFixed(1)}`} disabled />
             </div>
 
             <div className='mt-auto border-t-[1px] p-4'>
-                <p className='text-lg'>Formula:</p>
+                <p className='text-lg font-semibold'>Formula:</p>
                 <p className='text-sm'>x = A sin(at + φ)</p>
                 <p className='text-sm'>y = B sin(bt)</p>
             </div>
